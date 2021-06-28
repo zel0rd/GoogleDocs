c6ad0814c91b24726f16acd4a06a43ddb98fe451
younggil <zelord.kwoun@gmail.com>
2021-06-28 10:38:34 +0900

    [ADD] editor modules

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -2,6 +2,17 @@ import { useCallback, useRef } from 'react'
 import Quill from "quill"
 import "quill/dist/quill.snow.css"
 
+const TOOLBAR_OPTIONS = [
+    [{ header: [1,2,3,4,5,6,false]}],
+    [{ font: [] }],
+    [{ list: "ordered"}, {list: "bullet"}],
+    [{ color: [] }, { background: []}],
+    [{ script: "sub"}, {script: "super"}],
+    [{ align: []}],
+    ["image","blockquote","code-block"],
+    ["clean"],
+]
+
 export default function TextEditor() {
     const wrapperRef = useCallback((wrapper) => {
         if (wrapper == null) return 
@@ -9,9 +20,9 @@ export default function TextEditor() {
         wrapper.innerHTML = ''
         const editor = document.createElement('div')
         wrapper.append(editor)
-        new Quill("#container", {theme: 'snow'})
+        new Quill(editor, {theme: 'snow', modules: { toolbar:TOOLBAR_OPTIONS }})
         // asfdlaksdjf
 
     }, [])
-    return <div id="container" ref={wrapperRef}></div>
+    return <div className="container" ref={wrapperRef}></div>
 }
