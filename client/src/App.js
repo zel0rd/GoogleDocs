f957f34182cad7667c294d82e7a8fcb6be727e10
younggil <zelord.kwoun@gmail.com>
2021-06-28 10:00:51 +0900

    [ADD] quill text editor

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import TextEditor from "./TextEditor";
+
+
+function App() {
+  return <TextEditor />
+}
+
+export default App;
---
+++ client/src/TextEditor.js
@@ -0,0 +1,17 @@
+import { useCallback, useRef } from 'react'
+import Quill from "quill"
+import "quill/dist/quill.snow.css"
+
+export default function TextEditor() {
+    const wrapperRef = useCallback((wrapper) => {
+        if (wrapper == null) return 
+
+        wrapper.innerHTML = ''
+        const editor = document.createElement('div')
+        wrapper.append(editor)
+        new Quill("#container", {theme: 'snow'})
+        // asfdlaksdjf
+
+    }, [])
+    return <div id="container" ref={wrapperRef}></div>
+}
